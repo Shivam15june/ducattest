2a4c95575738d5d49cd0c1830f10f6269fa8a6e8
shivam <shivamsri15june@gmail.com>
2023-05-27 12:47:15 +0530

    feat file upload

---
+++ src/test/java/windowhandle/demo.java
@@ -0,0 +1,27 @@
+package windowhandle;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class demo {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+		driver.manage().window().maximize();
+
+		Thread.sleep(2000);
+
+		String windowid = driver.getWindowHandle();
+		System.out.println(windowid);
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
